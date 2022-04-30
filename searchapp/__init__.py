14f1b7559efdfff4abb4c9de51543e6af2d718d6
Alexander Lyotov <sippie84@gmail.com>
2022-04-30 18:03:39 +0300

    feature/db init app

--- searchapp/__init__.py
+++ searchapp/__init__.py
@@ -10,6 +10,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # db.init_app(app)
+    db.init_app(app)
     # app.register_blueprint(routes, url_prefix='/api/v1/text_docs/')
     return app
