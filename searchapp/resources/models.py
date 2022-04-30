a1b0a7d5ead8a92960f11f3c8e0fa9ff0188da2f
Alexander Lyotov <sippie84@gmail.com>
2022-04-30 16:46:34 +0300

    feature/post model draft

---
+++ searchapp/resources/models.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Books(db.Model):  # type: ignore
+    uid = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, nullable=False)
+    rubrics = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.String, nullable=False)
+
+
+@dataclass
+class Post:
+    id: int
+    rubrics: str
+    text: str
+    date_created: str
