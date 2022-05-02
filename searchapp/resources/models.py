287d9d1c8172b706d48d729199c99f76851fd8e0
Alexander Lyotov <sippie84@gmail.com>
2022-05-02 16:02:51 +0300

    feature/create_date field type changed to DateTime

--- searchapp/resources/models.py
+++ searchapp/resources/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,11 +10,11 @@ class Posts(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     rubrics = db.Column(db.String, nullable=False)
     text = db.Column(db.String, nullable=False)
-    created_date = db.Column(db.String, nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False)
 
 
 @dataclass
 class Post:
     rubrics: str
     text: str
-    created_date: str
+    created_date: datetime
