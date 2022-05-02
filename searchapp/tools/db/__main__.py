bdebcfba0e8a8603b3c5606841cbe1e6ab8b4a81
Alexander Lyotov <sippie84@gmail.com>
2022-05-02 14:54:50 +0300

    feature/fill db from cli

--- searchapp/tools/db/__main__.py
+++ searchapp/tools/db/__main__.py
@@ -1,6 +1,6 @@
 import typer
 
-from searchapp.tools.db import create_db
+from searchapp.tools.db import create_db, fill_db_initially
 
 app = typer.Typer()
 
@@ -12,9 +12,9 @@ def create():
     typer.echo('The process of creation is over. See logs for details.')
 
 
-# @app.command()
-# def fill(csv: str = typer.Argument('books.csv')):  # noqa: WPS404, B008
-#     populate_db_initially.populate_db_from_given_csv(csv_path=csv)
+@app.command()
+def fill(csv: str = typer.Argument('posts.csv')):  # noqa: WPS404, B008
+    fill_db_initially.fill_db_from_given_csv(csv_path=csv)
 
 
 if __name__ == '__main__':
