03e275dc7796962e1ce7ca3db3ec33d8e98b001a
Alexander Lyotov <sippie84@gmail.com>
2022-05-02 15:45:36 +0300

    feature/models and dataclass fields fixes

--- searchapp/resources/models.py
+++ searchapp/resources/models.py
@@ -6,16 +6,14 @@ db = SQLAlchemy()
 
 
 class Posts(db.Model):  # type: ignore
-    uid = db.Column(db.Integer, primary_key=True)
-    id = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     rubrics = db.Column(db.String, nullable=False)
     text = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.String, nullable=False)
+    created_date = db.Column(db.String, nullable=False)
 
 
 @dataclass
 class Post:
-    id: int
     rubrics: str
     text: str
-    date_created: str
+    created_date: str
