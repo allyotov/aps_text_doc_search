1f4f32dd004a415af21320628f4f06d21f987b62
Alexander Lyotov <sippie84@gmail.com>
2022-05-03 16:19:38 +0300

    feature/add bluprint

--- searchapp/__init__.py
+++ searchapp/__init__.py
@@ -4,12 +4,12 @@ import os
 from flask import Flask
 
 from searchapp.resources.models import db
-# from searchapp.resources.routes.routes import routes
+from searchapp.resources.routes.routes import routes
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    # app.register_blueprint(routes, url_prefix='/api/v1/text_docs/')
+    app.register_blueprint(routes, url_prefix='/api/v1/text_docs/')
     return app
