594a686760e62d3a3e5a7cd10e4cf89693f1c0a0
Alexander Lyotov <sippie84@gmail.com>
2022-05-03 15:01:14 +0300

    feature/text field hash index

--- searchapp/resources/models.py
+++ searchapp/resources/models.py
@@ -2,16 +2,37 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from sqlalchemy import text, cast
+from sqlalchemy.dialects import postgresql
 
 db = SQLAlchemy()
 
 
+def create_tsvector(*args):
+    exp = args[0]
+    for e in args[1:]:
+        exp += ' ' + str(e)
+    return func.to_tsvector('english', exp)
+
 class Posts(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True, index=True)
     rubrics = db.Column(db.String, nullable=False)
-    text = db.Column(db.String, nullable=False, index=True)
+    text = db.Column(db.UnicodeText, nullable=False)
     created_date = db.Column(db.DateTime, nullable=False)
 
+
+    __ts_vector__ = create_tsvector(
+        cast(func.coalesce(text, ''), postgresql.TEXT)
+    )
+
+    __table_args__ = (
+        db.Index(
+            'idx_text_fts',
+            __ts_vector__,
+            postgresql_using='gin'
+        ),
+    )
 
 @dataclass
 class Post:
