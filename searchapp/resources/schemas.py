a678b5d5db3de553007e281ca31292240e0a1f55
Alexander Lyotov <sippie84@gmail.com>
2022-05-04 23:02:48 +0300

    feature/list of str of rubrics

--- searchapp/resources/schemas.py
+++ searchapp/resources/schemas.py
@@ -3,6 +3,6 @@ from marshmallow import Schema, fields
 
 class PostSchema(Schema):
     id = fields.Int(required=True)
-    rubrics = fields.Str(required=True)
+    rubrics = fields.List(fields.Str(), required=True)
     text = fields.Str(required=True)
     created_date = fields.DateTime(required=True)
